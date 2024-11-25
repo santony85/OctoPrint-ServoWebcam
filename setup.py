d45d06be933fd2ae9e10f29b022f48f78bb07ce2
Antony <antony_santero@hotmail.com>
2024-11-25 21:50:48 +0100

    first name

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@
 ### Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "slatimelapse"
+plugin_identifier = "servowebcam"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
 plugin_package = "octoprint_slatimelapse"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "Sla Timelapse"
+plugin_name = "Servo Webcam"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
 plugin_version = "1.0.0"
