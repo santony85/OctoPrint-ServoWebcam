f310f0f60c69b9df8ccfb7f776452925592f2176
Antony <antony_santero@hotmail.com>
2024-11-25 17:50:00 +0100

    First commit

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ plugin_author = "Your Name"
 plugin_author_email = "you@somewhere.net"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/yourGithubName/OctoPrint-Helloworld"
+plugin_url = "https://github.com/santony85/OctoPrint-ServoWebcam"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
