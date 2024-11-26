0d4f1ca8117504aa7b0e5473ce43d1c7a12b721f
Antony <antony_santero@hotmail.com>
2024-11-26 17:34:00 +0100

    tout change

--- aold/setup.py
+++ aold/setup.py
@@ -4,49 +4,47 @@
 ### Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "EasyServo"
+plugin_identifier = "servowebcam"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
-plugin_package = "EasyServo"
+plugin_package = "octoprint_servowebcam"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "Easy Servo"
+plugin_name = "Servo Webcam"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.2.4"
+plugin_version = "1.0.0"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
-plugin_description = "Here is a growing plugin you can use to easily control servo motors using the octoprint interface"
+plugin_description = """Servo pluging for resin printers"""
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
-plugin_author = "iFrostizz, mledan"
+plugin_author = "Asantero.tech"
 
 # The plugin's author's mail address.
-plugin_author_email = "mlad89@gmail.com"
+plugin_author_email = "you@somewhere.net"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/mledan/OctoPrint-EasyServo"
+plugin_url = "https://github.com/santony85/OctoPrint-ServoWebcam"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = ["pigpio", "pantilthat"]
+plugin_requires = ["RPi.GPIO","pigpio", "pantilthat"]
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
 ### --------------------------------------------------------------------------------------------------------------------
 
 # Additional package data to install for this plugin. The subfolders "templates", "static" and "translations" will
-# already be installed automatically if they exist. Note that if you add something here you'll also need to update
-# MANIFEST.in to match to ensure that python setup.py sdist produces a source distribution that contains all your
-# files. This is sadly due to how python's setup.py works, see also http://stackoverflow.com/a/14159430/2028598
+# already be installed automatically if they exist.
 plugin_additional_data = []
 
-# Any additional python packages you need to install with your plugin that are not contained in <plugin_package>.*
-plugin_additional_packages = []
+# Any additional python packages you need to install with your plugin that are not contains in <plugin_package>.*
+plugin_addtional_packages = []
 
 # Any python packages within <plugin_package>.* you do NOT want to install with your plugin
 plugin_ignored_packages = []
@@ -84,7 +82,7 @@ setup_parameters = octoprint_setuptools.create_plugin_setup_parameters(
 	url=plugin_url,
 	license=plugin_license,
 	requires=plugin_requires,
-	additional_packages=plugin_additional_packages,
+	additional_packages=plugin_addtional_packages,
 	ignored_packages=plugin_ignored_packages,
 	additional_data=plugin_additional_data
 )
