d00e3d21348d694aea47a6f53950a9b8a0e7913e
Antony <antony_santero@hotmail.com>
2024-11-27 19:13:14 +0100

    no spaces

--- octoprint_servowebcam/__init__.py
+++ octoprint_servowebcam/__init__.py
@@ -5,44 +5,52 @@ import octoprint.plugin
 from gpiozero import Servo
 from time import sleep
 
+import flask
+
 class ServoWebcamdPlugin(octoprint.plugin.SettingsPlugin,
-					  octoprint.plugin.AssetPlugin,
-					  octoprint.plugin.TemplatePlugin,
-					  octoprint.plugin.StartupPlugin,
-					  octoprint.plugin.ShutdownPlugin,
-					  octoprint.plugin.SimpleApiPlugin):
-	def on_after_startup(self):
-		self._logger.info("Servo Webcam! (more: %s)" % self._settings.get(["url"]))
+                      octoprint.plugin.AssetPlugin,
+                      octoprint.plugin.TemplatePlugin,
+                      octoprint.plugin.StartupPlugin,
+                      octoprint.plugin.ShutdownPlugin,
+                      octoprint.plugin.SimpleApiPlugin):
+    def __init__(self):
+        self.sX = None
+        self.sY = None
+        
+    def on_after_startup(self):
+        self._logger.info("Servo Webcam! (more: %s)" % self._settings.get(["url"]))
 
-	def get_settings_defaults(self):
-		return dict(url="https://en.wikipedia.org/wiki/Hello_world")
+    def get_settings_defaults(self):
+        return dict(url="https://en.wikipedia.org/wiki/Hello_world")
 
-	def get_template_configs(self):
-		return [
-			dict(type="navbar", custom_bindings=False),
+    def get_template_configs(self):
+        return [
+            dict(type="navbar", custom_bindings=False),
             dict(type="generic", template="servowebcam.jinja2", custom_bindings=True),
-			dict(type="settings", custom_bindings=False)
-		]
+            dict(type="settings", custom_bindings=False)
+        ]
 
-	def get_assets(self):
-		return dict(
-			js=["js/servowebcam.js"]
-			#css=["css/helloworld.css"]
-			#less=["less/helloworld.less"]
-		)
-
+    def get_assets(self):
+        return dict(
+            js=["js/servowebcam.js"]
+            #css=["css/helloworld.css"]
+            #less=["less/helloworld.less"]
+        )
+    def on_api_get(self, request):
+       return flask.jsonify(foo="bar")
+   
 __plugin_name__ = "Servo Webcam"
 __plugin_pythoncompat__ = ">=2.7,<4"
 
 def __plugin_load__():
-	global __plugin_implementation__
-	__plugin_implementation__ = ServoWebcamdPlugin()
+    global __plugin_implementation__
+    __plugin_implementation__ = ServoWebcamdPlugin()
 
-	global __plugin_hooks__
-	__plugin_hooks__ = {
-		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information,
-		"octoprint.comm.protocol.gcode.received": __plugin_implementation__.process_gcode,
-		"octoprint.comm.protocol.gcode.sending": __plugin_implementation__.read_gcode
-	}
+    global __plugin_hooks__
+    __plugin_hooks__ = {
+        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information,
+        "octoprint.comm.protocol.gcode.received": __plugin_implementation__.process_gcode,
+        "octoprint.comm.protocol.gcode.sending": __plugin_implementation__.read_gcode
+    }
 
 
