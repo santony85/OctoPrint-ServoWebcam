3d6dec81edc07e6b7ba4e797cc8fb0c86f7d3586
Antony <antony_santero@hotmail.com>
2024-11-28 12:43:38 +0100

    add setup

--- octoprint_servowebcam/__init__.py
+++ octoprint_servowebcam/__init__.py
@@ -61,9 +61,8 @@ class ServoWebcamdPlugin(octoprint.plugin.SettingsPlugin,
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BCM)
         try:
-                ppp=0
-                #GPIO.setup(12, GPIO.OUT)
-                #GPIO.setup(13, GPIO.OUT)
+                GPIO.setup(12, GPIO.OUT)
+                GPIO.setup(13, GPIO.OUT)
                 
         except Exception:
                 self._logger.exception(
