59ee70c520b3887c5d96aebc923cb93bfd2e0889
Antony <antony_santero@hotmail.com>
2024-12-12 19:51:12 +0100

    saved

--- octoprint_servowebcam/__init__.py
+++ octoprint_servowebcam/__init__.py
@@ -10,7 +10,7 @@ import octoprint.plugin
 import threading
 
 import flask
-from gpiozero import Servo
+
 
 
 class ServoWebcamdPlugin(octoprint.plugin.SettingsPlugin,
@@ -22,18 +22,6 @@ class ServoWebcamdPlugin(octoprint.plugin.SettingsPlugin,
     def __init__(self):
         try:
             p=0
-            #self.myServo = Servo(13)
-            #self._hasGPIO = True
-            """global GPIO
-            from RPi import GPIO
-            self._hasGPIO = True
-            GPIO.setmode(GPIO.BCM)
-            GPIO.setup(12, GPIO.OUT)
-            GPIO.setup(13, GPIO.OUT)
-            self.pX = GPIO.PWM(12, 50) # GPIO 17 for PWM with 50Hz
-            self.pX.start(2.5) # Initialization
-            self.pY = GPIO.PWM(12, 50) # GPIO 17 for PWM with 50Hz
-            self.pY.start(2.5) # Initialization"""
         except (ImportError, RuntimeError):
             self._hasGPIO = False
 
@@ -45,37 +33,15 @@ class ServoWebcamdPlugin(octoprint.plugin.SettingsPlugin,
     def get_settings_defaults(self):
         return dict(
             url="https://en.wikipedia.org/wiki/Hello_world",
-            hasGPIO=self._hasGPIO
+            
             )
         
     def on_settings_initialized(self):
         self._logger.error("Error importing RPi.GPIO. OK")
-        self.configure_gpio()
-        
-    def configure_gpio(self):
-        if not self._hasGPIO:
-            self._logger.error("Error importing RPi.GPIO.")
-            return
-        
-        """GPIO.setwarnings(False)
-        GPIO.setmode(GPIO.BCM)"""
-        
-        try:
-                ppp=0
-                """GPIO.setup(12, GPIO.OUT)
-                GPIO.setup(13, GPIO.OUT)
-                self.p = GPIO.PWM(12, 50) # GPIO 17 for PWM with 50Hz
-                self.p.start(2.5) # Initialization"""
                 
-        except Exception:
-                self._logger.exception(
-                    "Exception while setting up GPIO pin 12 13"
-                )
-        
     def get_template_configs(self):
         return [
             dict(type="navbar", custom_bindings=False),
-            dict(type="generic", template="servowebcam.jinja2", custom_bindings=True),
             dict(type="settings", custom_bindings=False)
         ]
 
