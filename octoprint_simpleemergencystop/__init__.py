1c7054a1fd2ed7c10c02131c22988be02792d9f1
Antony <antony_santero@hotmail.com>
2024-12-12 22:59:09 +0100

    lib

--- octoprint_simpleemergencystop/__init__.py
+++ octoprint_simpleemergencystop/__init__.py
@@ -2,6 +2,11 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import octoprint.plugin
+import asyncio
+import logging
+import random
+
+from nextion import TJC, EventType
 
 
 class SimpleemergencystopPlugin(octoprint.plugin.StartupPlugin,
